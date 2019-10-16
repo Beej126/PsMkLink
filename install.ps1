3e225e995d1b41fd2bd84bfe6970fda85721a8cf
Beej <6301228+Beej126@users.noreply.github.com>
2019-10-16 11:40:48 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,7 +8,7 @@ Write-Host "Creating module directory: $installPath"
 New-Item -Type Container -Force -path $installPath | out-null
 
 Write-Host "Downloading and installing"
-(new-object net.webclient).DownloadString($download) | Out-File "$installPath`.psm1" 
+(new-object net.webclient).DownloadString($download) | Out-File "$installPath\$command`.psm1" 
 
 Write-Host "Installed!"
 Write-Host "Use `"Import-Module $command`" and then `"$command`""
